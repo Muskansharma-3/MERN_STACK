2b9ae441185c9b4ba062a11f201e56cf3ed1e3f2
Muskan <muskansharma0203@gmail.com>
2025-06-10 18:42:21 +0530

    changes

--- react-project-one/src/components/UserCard.jsx
+++ react-project-one/src/components/UserCard.jsx
@@ -6,7 +6,7 @@ const UserCard = () => {
   return (
     <div className='user-container'>
         <p id='user-name'>Muskan Sharma</p>
-        <img id='user-img' src={mypic} alt="Muskan" />
+        <img id='user-img' src={mypic} alt="MuskanPic" />
         <p id='user-desc'>Decription of Muskan Sharma</p>
     </div>
   )
