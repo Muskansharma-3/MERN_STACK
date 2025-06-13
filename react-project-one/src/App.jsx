072ebed53e38ae5c302fedca94700efdb79dd258
Muskan <muskansharma0203@gmail.com>
2025-06-13 09:45:03 +0530

    changes

--- react-project-one/src/App.jsx
+++ react-project-one/src/App.jsx
@@ -6,9 +6,9 @@ function App() {
 
   return (
     <div className="container">
-      <UserCard/>
-      <UserCard/>
-      <UserCard/>
+      <UserCard name="Muskan Sharma"/>
+      <UserCard name="Shaweta Sharma"/>
+      <UserCard name="Kanvi Sharma"/>
     </div>
   )
 }
--- react-project-one/src/components/UserCard.jsx
+++ react-project-one/src/components/UserCard.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import mypic from '../assets/mypic.webp'
 import "./UserCard.css"
 
-const UserCard = () => {
+const UserCard = (props) => {
   return (
     <div className='user-container'>
-        <p id='user-name'>Muskan Sharma</p>
+        <p id='user-name'>{props.name}</p>
         <img id='user-img' src={mypic} alt="Muskan" />
         <p id='user-desc'>BTech CSE Student</p>
     </div>
