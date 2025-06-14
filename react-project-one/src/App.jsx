316160e259ede36d9bfb6bbe98e1d7c1e7797122
Muskan <muskansharma0203@gmail.com>
2025-06-14 12:39:27 +0530

    changes

--- react-project-one/src/App.jsx
+++ react-project-one/src/App.jsx
@@ -3,14 +3,17 @@ import UserCard from "./components/UserCard"
 import UseState from './components/UseState'
 import Card from './components/Card'
 import Button from './components/Button'
+import StateLifting from './components/StateLifting'
 
 
 function App() {
-  const [count, setCount]=useState(0);
-  function handleClick(){
-    setCount(count + 1);
+  // const [count, setCount]=useState(0);
+  // function handleClick(){
+  //   setCount(count + 1);
     
-  }
+  // }
+
+  const[name, setName] = useState('');
 
   return (
     <div className="container">
@@ -22,11 +25,15 @@ function App() {
         <p>BTech CSE Student</p>
       </Card> */}
 
-      <Button handleClick={handleClick}>
+      {/* <Button handleClick={handleClick}>
         <h1>{count}</h1>
-        </Button>
+        </Button> */}
 
       {/* <UseState/> */}
+
+      <StateLifting title="Card1" name={name} setName={setName}/>
+      <StateLifting title="Card2" name={name} setName={setName}/>
+      {/* <p>I am inside parent component and the value of name is {name}</p> */}
     </div>
   )
 }
