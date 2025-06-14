25268863b123a2e02764fde2f9ef000e3bd9c792
Muskan <muskansharma0203@gmail.com>
2025-06-14 13:12:30 +0530

    change

---
+++ react-project-one/src/components/Login.jsx
@@ -0,0 +1,12 @@
+// Conditional Remdering -> Login and Logout
+
+import { button } from 'framer-motion/client'
+import React from 'react'
+
+const Login = () => {
+  return (
+    <button>Login</button>
+  )
+}
+
+export default Login
