f8e652e9c3f66c584c5e50fd0ce8f24356ace257
Muskan <muskansharma0203@gmail.com>
2025-06-14 12:17:28 +0530

    changes

--- react-project-one/src/App.jsx
+++ react-project-one/src/App.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, {useState} from 'react'
 import UserCard from "./components/UserCard"
 import UseState from './components/UseState'
 import Card from './components/Card'
+import Button from './components/Button'
 
 
 function App() {
+  const [count, setCount]=useState(0);
+  function handleClick(){
+    setCount(count + 1);
+    
+  }
 
   return (
     <div className="container">
       {/* <UserCard name="Muskan Sharma" desc="desc1" />
       <UserCard name="Shaweta Sharma" desc="desc2"/>
       <UserCard name="Kanvi Sharma" desc="desc3"/> */}
-      <Card>
+      {/* <Card>
         <h1>Muskan Sharma</h1>
         <p>BTech CSE Student</p>
-      </Card>
+      </Card> */}
+
+      <Button handleClick={handleClick}>
+        <h1>{count}</h1>
+        </Button>
 
       {/* <UseState/> */}
     </div>
