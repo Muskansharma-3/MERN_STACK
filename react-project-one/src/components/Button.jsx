fa12d8c699057e2d75dece87d2418e9f5c5b4698
Muskan <muskansharma0203@gmail.com>
2025-06-21 18:43:42 +0530

    change

--- react-project-one/src/components/Button.jsx
+++ react-project-one/src/components/Button.jsx
@@ -4,7 +4,7 @@ const Button = (props) => {
   return (
     <div>
         {props.children}
-        <button onClick={props.handleClick}>Click me</button>
+        <button onClick={props.handleClick}>Click me!</button>
     </div>
   )
 }
