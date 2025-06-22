d175ade34e70602a83085665c7f26fc1e7c4baa1
Muskan <muskansharma0203@gmail.com>
2025-06-22 14:17:44 +0530

    change

--- react-project-one/src/components/StateLifting.jsx
+++ react-project-one/src/components/StateLifting.jsx
@@ -4,7 +4,7 @@ const StateLifting = (props) => {
   return (
     <div>
         <input type="text" onChange={(e)=>props.setName(e.target.value)} />
-        <p>Name state variable value in {props.title} : {props.name}</p>
+        <p>Name state variable value in {props.title}: {props.name}</p>
     </div>
   )
 }
